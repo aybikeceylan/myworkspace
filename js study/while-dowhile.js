04063926e25ac2d7ecf7a74746c3ef2b68126087
aybike <aybikesayar@gmail.com>
2022-08-17 22:27:20 +0300

    cmmt

--- js study/while-dowhile.js
+++ js study/while-dowhile.js
@@ -4,15 +4,26 @@
 
 const not = +prompt("lütfen 0-100 arasında bir not giriniz")
 
-while(not<0 || not>100){
+while (not < 0 || not > 100) {
     console.log("not 0-100 arasında olmalıdır");
     not = +prompt("lütfen 0-100 arasında bir not giriniz")
 }
-console.log("girdiğiniz not:",not);
+console.log("girdiğiniz not:", not);
 
 
 
 // if (not<0 || not>100) {
 //     alert("not 0-100 arasında olmalıdır")
-    
+
 // }
+
+//? ORNEK: Kullanicidan 0-100 arasinda bir not isteyen ve girilen not
+//? 0-100'den farkli ise Kullaciniyi uyararak yeniden 0-100 arasinda
+//? not girmeye zorlayan kodu do-while dongusu ile yaziniz
+
+let not1;
+do {
+    not1 = +prompt("lütfen 0-100 arasında bir not giriniz")
+} while (not1 < 0 || not1 > 100)
+
+console.log("girdiğiniz not:", not1);
