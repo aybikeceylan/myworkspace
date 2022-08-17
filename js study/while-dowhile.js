239e6687102ff852dcdedcfc9f5d6664e84bef01
aybike <aybikesayar@gmail.com>
2022-08-17 22:45:37 +0300

    while

--- js study/while-dowhile.js
+++ js study/while-dowhile.js
@@ -21,9 +21,19 @@ console.log("girdiğiniz not:", not);
 //? 0-100'den farkli ise Kullaciniyi uyararak yeniden 0-100 arasinda
 //? not girmeye zorlayan kodu do-while dongusu ile yaziniz
 
-let not1;
-do {
-    not1 = +prompt("lütfen 0-100 arasında bir not giriniz")
-} while (not1 < 0 || not1 > 100)
+// let not1;
+// do {
+//   not1 = +prompt("Lutfen 0-100 arasinda bir not girniz.");
+//   if (not1 < 0 || not1 > 100) {
+//     console.log("Not 0-100 arasinda olmaldir.");
+//   }
+// } while (not1 < 0 || not1 > 100);
 
-console.log("girdiğiniz not:", not1);
+// console.log("Giridiginiz Not:", not1);
+
+
+// ÖDEV: Klavyeden Q veya q karakteri girilene kadar not girişi yapan ve bu karakterlerden birisi girildiğinde O ana kadar girilen tüm notların ortalamasını hesaplayarak konsola ortalamayı bastıran uygulamayı yazınız.
+
+
+
+
