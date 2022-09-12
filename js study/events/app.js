df1c2828a83667fbdcdb9f7e600a980da9c69782
aybike <aybikesayar@gmail.com>
2022-09-12 22:33:43 +0300

    v

--- js study/events/app.js
+++ js study/events/app.js
@@ -6,3 +6,12 @@ const langList = document.getElementById("lang-list")
 
 const newUl = document.createElement("ul");
 langList.appendChild(newUl)
+
+
+addBtn.addEventListener('click', () => {
+    if (!langInput.value) {
+        alert("please enter a language")
+    } else {
+        newUl.innerHTML = `<li>${langInput.value}<li>`
+    }
+})
